bf0c4fc6703673ba95b8ba66d6e2fb86fc9378f4
Jose <rmjosea@gmail.com>
2015-09-23 21:41:15 +0200

    Final Code with example

--- cachematrix.R
+++ cachematrix.R
@@ -43,3 +43,8 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m
 }
+
+##EXAMPLES 
+## testData <- matrix(c(1,2,3, 0,1,4, 5,6,0), nrow = 3, ncol = 3)
+## Cache <- makeCacheMatrix(testData)
+## Inverse <- cacheSolve(Cache)
